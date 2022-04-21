7012dc6f01f141833bfb54104b771d09b097c186
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2022-04-21 17:00:05 +0100

    Update chloroWrapper.sh

--- chloroWrapper.sh
+++ chloroWrapper.sh
@@ -4,27 +4,27 @@
 
 cd chloroColocalisations
 
-./distanceMitoChloro.R GFP1-mitos.xml GFP1-chloros.xml 546.017
-./distanceMitoChloro.R GFP2-mitos.xml GFP2-chloros.xml 737.903
-./distanceMitoChloro.R GFP3-mitos.xml GFP3-chloros.xml 3224.905
-./distanceMitoChloro.R GFP6-mitos.xml GFP6-chloros.xml 1125.252
-./distanceMitoChloro.R GFP10-mitos.xml GFP10-chloros.xml 1504.609
-./distanceMitoChloro.R GFP11-mitos.xml GFP11-chloros.xml 615.274
-./distanceMitoChloro.R GFP12-mitos.xml GFP12-chloros.xml 1113.034
-./distanceMitoChloro.R GFP4-mitos.xml GFP4-chloros.xml 1202.689
-./distanceMitoChloro.R GFP5-mitos.xml GFP5-chloros.xml 1590.431
-./distanceMitoChloro.R GFP8-mitos.xml GFP8-chloros.xml 2697.122
+./distanceMitoChloro.r GFP1-mitos.xml GFP1-chloros.xml 546.017
+./distanceMitoChloro.r GFP2-mitos.xml GFP2-chloros.xml 737.903
+./distanceMitoChloro.r GFP3-mitos.xml GFP3-chloros.xml 3224.905
+./distanceMitoChloro.r GFP6-mitos.xml GFP6-chloros.xml 1125.252
+./distanceMitoChloro.r GFP10-mitos.xml GFP10-chloros.xml 1504.609
+./distanceMitoChloro.r GFP11-mitos.xml GFP11-chloros.xml 615.274
+./distanceMitoChloro.r GFP12-mitos.xml GFP12-chloros.xml 1113.034
+./distanceMitoChloro.r GFP4-mitos.xml GFP4-chloros.xml 1202.689
+./distanceMitoChloro.r GFP5-mitos.xml GFP5-chloros.xml 1590.431
+./distanceMitoChloro.r GFP8-mitos.xml GFP8-chloros.xml 2697.122
 
-./distanceMitoChloro.R MSH6-mitos.xml MSH6-chloros.xml 1789.144
-./distanceMitoChloro.R MSH17-mitos.xml MSH17-chloros.xml 1008.383
-./distanceMitoChloro.R MSH19-mitos.xml MSH19-chloros.xml 1364.212
-./distanceMitoChloro.R MSH20-mitos.xml MSH20-chloros.xml 807.691
-./distanceMitoChloro.R MSH22-mitos.xml MSH22-chloros.xml 1250.798
-./distanceMitoChloro.R MSH26-mitos.xml MSH26-chloros.xml 851.658
-./distanceMitoChloro.R MSH7-mitos.xml MSH7-chloros.xml 1651.684
-./distanceMitoChloro.R MSH13-mitos.xml MSH13-chloros.xml 885.533
-./distanceMitoChloro.R MSH25-mitos.xml MSH25-chloros.xml 982.454
-./distanceMitoChloro.R MSH10-mitos.xml MSH10-chloros.xml 2983.425
+./distanceMitoChloro.r MSH6-mitos.xml MSH6-chloros.xml 1789.144
+./distanceMitoChloro.r MSH17-mitos.xml MSH17-chloros.xml 1008.383
+./distanceMitoChloro.r MSH19-mitos.xml MSH19-chloros.xml 1364.212
+./distanceMitoChloro.r MSH20-mitos.xml MSH20-chloros.xml 807.691
+./distanceMitoChloro.r MSH22-mitos.xml MSH22-chloros.xml 1250.798
+./distanceMitoChloro.r MSH26-mitos.xml MSH26-chloros.xml 851.658
+./distanceMitoChloro.r MSH7-mitos.xml MSH7-chloros.xml 1651.684
+./distanceMitoChloro.r MSH13-mitos.xml MSH13-chloros.xml 885.533
+./distanceMitoChloro.r MSH25-mitos.xml MSH25-chloros.xml 982.454
+./distanceMitoChloro.r MSH10-mitos.xml MSH10-chloros.xml 2983.425
 
 cd ..
 
